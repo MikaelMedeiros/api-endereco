e6a9df38410fbeef2dfb678f0fe015dbd5548df3
Mikael Medeiros <mikaeltruth@gmail.com>
2021-04-18 14:44:24 -0300

    Tratamento da promise ao conectar no mongodb e implementacao das rotas de post e get

--- src/controllers/estado.controller.js
+++ src/controllers/estado.controller.js
@@ -2,12 +2,28 @@ const express = require('express');
 const router = express.Router();
 const Estado = require('../models/estado.model');
 
-router.get('/', (req, res) => {
-    res.send('Estado!');
+router.get('/', async (req, res) => {
+    try {
+        const estados = await Estado.find( {} );        
+        return res.send(estados);   
+    } catch (error) {
+        return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
+    }    
 });
 
-router.post('/', (req, res) => {    
-    return res.send(req.body);
+router.post('/', async (req, res) => { 
+    try {
+        const { sigla } = req.body;
+
+        if(await Estado.findOne({ sigla }))
+            return res.status(400).send({ status: res.statusCode, erro: 'Já existe um estado com essa sigla.' });    
+
+        const estado = await Estado.create(req.body);
+
+        return res.send({ estado });
+    }  catch(erro) {
+        return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
+    } 
 });
 
 module.exports = app => app.use('/estados', router);
--- src/database/mongo.db.js
+++ src/database/mongo.db.js
@@ -2,10 +2,20 @@ const mongoose = require('mongoose');
 require('dotenv/config');
 
 mongoose.connect(
-    process.env.DB_CONNECTION,
-    { useNewUrlParser: true},
-    () => console.log('MongoDB: Estou conectado!')
-);
+        process.env.DB_CONNECTION, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        }
+    )
+    .then(() => {
+        console.log("MongoDB: Estou conectado!");
+    })
+    .catch(err => {
+        console.log("MongoDB: Não consegui me conectar :/", err);
+        process.exit();
+    });
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
