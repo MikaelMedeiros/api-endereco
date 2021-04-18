73ef78697f523d605a69c06d86d0c5d1205b5cf4
Mikael Medeiros <mikaeltruth@gmail.com>
2021-04-18 17:57:53 -0300

    Criação de CRUD, ainda sem muitas validações, para Cidade e Estado

--- src/controllers/cidade.controller.js
+++ src/controllers/cidade.controller.js
@@ -2,12 +2,63 @@ const express = require('express');
 const router = express.Router();
 const Cidade = require('../models/cidade.model');
 
-router.get('/', (req, res) => {
-    res.send('Cidade!');
+router.get('/', async (req, res) => {
+    try {
+        const cidades = await Cidade.find( {} );        
+        return res.send(cidades);   
+    } catch (erro) {
+        return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
+    }    
 });
 
-router.post('/', (req, res) => {
-    return res.send(req.body);
+router.post('/', async (req, res) => { 
+    try {
+        const { nome } = req.body;
+
+        if(await Cidade.findOne({ nome }))
+            return res.status(400).send({ status: res.statusCode, erro: 'Já existe uma cidade com esse nome.' });    
+
+        const cidade = await Cidade.create(req.body);
+        
+        return res.status(201).send({ cidade });
+    }  catch(erro) {
+        return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
+    } 
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;        
+        const cidade = await Cidade.findById(id);
+
+        if(!cidade) 
+            return res.status(404).send({ status: res.statusCode, erro: 'Essa cidade não existe.' });    
+        
+        return res.send(cidade);    
+    } catch (erro) {
+        return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
+    }    
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const filtro = { _id: req.params.id};
+        const cidadeAtualizado = { $set: req.body };
+        await Cidade.updateOne(filtro, cidadeAtualizado);        
+        return res.send({ status: res.statusCode, mensagem: 'Cidade atualizada.' });
+    } catch (erro) {
+        return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const filtro = { _id: req.params.id};              
+        await Cidade.deleteOne(filtro);
+        return res.send({ status: res.statusCode, mensagem: 'Cidade removida.' });
+    } catch (erro) {
+        return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
+    }
 });
 
 module.exports = app => app.use('/cidades', router);
--- src/controllers/estado.controller.js
+++ src/controllers/estado.controller.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
     try {
         const estados = await Estado.find( {} );        
         return res.send(estados);   
-    } catch (error) {
+    } catch (erro) {
         return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
     }    
 });
@@ -19,11 +19,46 @@ router.post('/', async (req, res) => {
             return res.status(400).send({ status: res.statusCode, erro: 'Já existe um estado com essa sigla.' });    
 
         const estado = await Estado.create(req.body);
-
-        return res.send({ estado });
+        
+        return res.status(201).send({ estado });
     }  catch(erro) {
         return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
     } 
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;        
+        const estado = await Estado.findById(id);
+
+        if(!estado) 
+            return res.status(404).send({ status: res.statusCode, erro: 'Esse estado não existe.' });    
+        
+        return res.send(estado);    
+    } catch (erro) {
+        return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
+    }    
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const filtro = { _id: req.params.id};
+        const estadoAtualizado = { $set: req.body };
+        await Estado.updateOne(filtro, estadoAtualizado);        
+        return res.send({ status: res.statusCode, mensagem: 'Estado atualizado.' });
+    } catch (erro) {
+        return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const filtro = { _id: req.params.id};              
+        await Estado.deleteOne(filtro);
+        return res.send({ status: res.statusCode, mensagem: 'Estado removido.' });
+    } catch (erro) {
+        return res.status(400).send({status: res.statusCode, mensagem: 'Requisição inválida', erro: erro});
+    }
 });
 
 module.exports = app => app.use('/estados', router);
